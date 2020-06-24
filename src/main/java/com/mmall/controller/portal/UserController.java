c4e9b2412968b9e717d39120efcc969cadbd6ef8
fzy <18790258213@163.com>
2020-06-24 15:08:46 +0800

    init

---
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -0,0 +1,12 @@
+package com.mmall.controller.portal;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Auther: fang
+ * @Dec:
+ * @Date: crate in 2:58 下午2020/6/24
+ */
+@Controller
+public class UserController {
+}
